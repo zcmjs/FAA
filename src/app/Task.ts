7cf188d7965ed0319194ea64d40d436643160953
zcmjs <zcmjs>
2020-10-04 18:54:18 +0200

    przykład property binding

---
+++ src/app/Task.ts
@@ -0,0 +1,5 @@
+export interface ITask {
+  name: string;
+  deadline: string;
+  done: boolean;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ITask} from './Task';
+import {DomSanitizer, SafeHtml, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  readonly notSanitizedScript = '<script>alert(1)</script>';
+  readonly sanitizedScript: any;
+  // taskName: string;
+  focusExample: string
   config: { [key: string]: string | Date } = null;
+  tasks: Array<ITask> = [
+    {
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false,
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2020-01-03',
+      done: false,
+    },
+    {
+      name: 'Sprzątanie kuwety',
+      deadline: '2020-01-04',
+      done: false,
+    },
+  ];
+  blackDiv: 'red';
 
-  constructor() {
-    setTimeout( () => {
+  clearTasks(): void {
+    this.tasks = [];
+  }
+
+  constructor(private sanitizer: DomSanitizer) {
+
+    this.sanitizedScript = sanitizer.bypassSecurityTrustHtml('<script>console.log(1)</script>');
+
+    setTimeout(() => {
       this.config = {
         title: 'Lista zadań',
         footer: '© Lista zadań zbudowana w Angularze.',
@@ -17,4 +48,28 @@ export class AppComponent {
       };
     }, 500);
   }
+
+  onKeyUp($event: KeyboardEvent) {
+    // każdy event generuje obiekt pod zmienną $event
+    // bez rzutowania, pole value nie byłoby widoczne
+    const target = $event.target as HTMLInputElement;
+    // this.taskName = target.value;
+  }
+
+  createTask(name: string, date: string): void {
+    const task: ITask = {
+      name,
+      deadline: date,
+      done: false
+    }
+    this.tasks.push(task);
+  }
+
+  createTask2(): void {
+    alert('createTask2');
+  }
+
+
+
+
 }
