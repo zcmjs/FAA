cb0e9ea96a1fd77a8e8abe745400e98f583b6c3f
zcmjs <zcmjs>
2020-11-01 13:38:33 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,56 +7,13 @@ import {filter, map, toArray} from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  taskName = 'Sugerowane codzienne zadanie';
+export class AppComponent {
 
-// Observable jest strumieniem danych wszelkiego rodzaju
-// Subskrybowanie oznacza podłączenie się do strumienia danych.
-// Podłączenie to zazwyczaj trwa cały czas i dane możemy otrzymywać w różnych odcinkach czasowych
-  basicObservable(): void {
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const observable: Observable<Array<number>> = of(numbers);
-    // of -tworzy obiekt observalbe
-
-
-    //jesli subscript nie bedziemy obserwowac, to nie otrzymamy informacji
-    observable.subscribe(x => {
-      console.log(x);
-    });
-    //podobną do działania Of jest funkcja from, która zwraca liste pojedynczych elementów, a nie tablicę elementów
-    //W środku metody pipe wstawiamy operatory
-    const observable2: Observable<number> = from(numbers);
-    observable2.pipe(
-      map(n => n * 3),
-      filter(n => n % 2 === 0),
-      // toArray()
-    ).subscribe(x => {
-      console.log(x);
-    });
+  constructor() {
   }
 
-  //Subjecty to specjalne observable, ktore powalazją dzielicdane na wielu subskrybentów
-  basicSubject(): void {
-    const subject = new Subject<number>();
+  add() {
 
-    subject.subscribe(x=>console.log(x));
-    subject.next(2137);
-
-    //behaviorSubject zawsze przetrzymuję ostatnią wartość lub wartośc initializacyjną
-    const bSubject = new BehaviorSubject<number>(666);
-    bSubject.next(999);
-    bSubject.subscribe(x=>console.log(x)); //999
-
-    //Każda subskrypcja pracuję w pamięci przeglądarki
-    subject.unsubscribe();
-    bSubject.unsubscribe();
-  }
-  //Observable vs subject. Observable jest tworzone nowe dla każdego. Każdy dostanię tą samą partię danych.
-  //Subject tworzy jedno observable dla wielu subskrybentów
-
-  ngOnInit(): void {
-    // this.basicObservable();
-    this.basicSubject();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { LeftComponent } from './left/left.component';
+import { RightComponent } from './right/right.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LeftComponent, RightComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
