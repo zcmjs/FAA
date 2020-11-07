5cdacebc5ea16e13cc1cb85e0cf770b5bf5cc9e1
zcmjs <zcmjs>
2020-11-07 21:07:05 +0100

    - ActivatedRoute, ParamMap możemy pobrać jako Observable albo jako zwykły obiekt z obiektu snapshot w ActivatedRoute, routerLink, serwis location

--- src/app/pages/categories/movies-in-category/movies-in-category.component.ts
+++ src/app/pages/categories/movies-in-category/movies-in-category.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Movie } from 'src/app/model/Movie';
+import { HttpService } from 'src/app/services/http.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movies-in-category',
@@ -11,7 +13,14 @@ export class MoviesInCategoryComponent implements OnInit {
 
   movies: Observable<Movie[]>;
 
-  constructor() {}
+  constructor(private httpService: HttpService,
+              private route: ActivatedRoute) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    //teraz param jest zwykłym obiektem, bo odwołaliśmy się przez snapshot (Pobieranie bez observable)
+    //Niestety takie rozwiazanie jest słabe ponieważ przy zmianie adresu(klik button next na html) z category/comedy ie nastapi odswiezenie, bonie nastapi inicjalizacja(dlatego lepiej zastosować subscription)
+    const category = this.route.snapshot.paramMap.get('category');
+    this.movies = this.httpService.getMoviesFromCategory(category);
+
+  }
 }
--- src/app/pages/movies/movie-details/movie-details.component.ts
+++ src/app/pages/movies/movie-details/movie-details.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Movie } from 'src/app/model/Movie';
 import { HttpService } from 'src/app/services/http.service';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movie-details',
@@ -9,16 +12,40 @@ import { HttpService } from 'src/app/services/http.service';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
-
   movieDetails: Observable<Movie>;
 
-  constructor(private http: HttpService) {
+  constructor(private http: HttpService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private location: Location) {
   }
 
   ngOnInit() {
+    //tutaj zastosowano fajny trik, przełanaczania (z observable map na http) że zwracana jest wartość Observable<Movie> a nie observable paramMap
+    //Dodatkjowo switchMap jeśli wykryję, że zmieniło się id to usunię stare http i zastapi nowym (pod warunkiem że stare http sie jeszcze nie skońćzyło)
+    this.movieDetails = this.route.paramMap.pipe(
+      //params.get('id') wyciągniecie parameteru z routingu
+      switchMap((params: ParamMap) => this.http.getMovie(params.get('id')))
+    )
   }
 
   goToMovies() {
+    // this.router.navigate(['/movies']);
+    this.location.back();
   }
+
+// - przy redirectTo musimy ustawić właściwość match
+//
+// - prefix oznacza ze routing sprawdza część url przed ścieżką
+//
+// - full oznacza dokładne dopasowanie
+//
+// - ActivatedRoute to serwis, który zawiera informacje o ścieżce, która kieruje do danego komponentu
+//
+// - ParamMap możemy pobrać jako Observable albo jako zwykły obiekt z obiektu snapshot w ActivatedRoute
+//
+// - oprócz routerLink możemy użyć metody navigate z serwisu Router. Serwis ten służy do nawigacji po routingu.
+//
+// - serwis location może nam posłużyć do poruszania się pomiędzy odwiedzanymi stronami.
 
 }
